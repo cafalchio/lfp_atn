561dcf249b70264e72e0ec71c9f65674ec274225
Sean Martin <seankieran.m@hotmail.com>
2021-04-26 12:48:45 +0100

    Test LFP groups more

--- tests/test_lfp_groups.py
+++ tests/test_lfp_groups.py
@@ -3,7 +3,8 @@ import simuran
 from pprint import pprint
 
 # Import from my files
-from lfp_atn_simuran.analysis.frequency_analysis import grouped_powers
+from lfp_atn_simuran.analysis.frequency_analysis import grouped_powers, powers
+from lfp_atn_simuran.analysis.lfp_clean import LFPClean
 
 # Establish the recording layout
 def recording_info():
@@ -108,10 +109,15 @@ def recording_info():
 def main(set_file_location, output_location):
     recording = simuran.Recording(
         params=recording_info(), base_file=set_file_location)
+    sigs = LFPClean.clean_lfp_signals(
+            recording, verbose=False, vis=False, append_avg=True
+        )
+    fig = sigs.plot(title=os.path.basename(set_file_location), duration=20, show=False)
+    fig.savefig(output_location[:-4] + ".png", dpi=300)
     analysis_handler = simuran.AnalysisHandler()
-    analysis_handler.add_fn(grouped_powers, recording, win_len=1)
+    analysis_handler.add_fn(grouped_powers, recording, min_f=1, max_f=100, win_len=2)
+    analysis_handler.add_fn(powers, recording, min_f=1, max_f=100, win_len=1)
     analysis_handler.run_all_fns()
-    pprint(analysis_handler.results)
     analysis_handler.save_results(output_location=output_location)
 
 
