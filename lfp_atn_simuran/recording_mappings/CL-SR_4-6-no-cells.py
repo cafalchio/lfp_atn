5b84e163737c88152b68dab36fc7a550be5964e6
Sean Martin <seankieran.m@hotmail.com>
2021-06-03 13:14:33 +0100

    Updates

--- lfp_atn_simuran/recording_mappings/CL-SR_4-6-no-cells.py
+++ lfp_atn_simuran/recording_mappings/CL-SR_4-6-no-cells.py
@@ -12,14 +12,14 @@ def setup_signals():
     """Set up the signals (such as eeg or lfp)."""
 
     # The total number of signals in the recording
-    num_signals = 0
+    num_signals = 32
 
     # What brain region each signal was recorded from
     regions = ["SUB"] * 2 + ["RSC"] * 2 + ["SUB"] * 28
 
     # If the wires were bundled, or any other kind of grouping existed
     # If no grouping, grouping = [i for i in range(num_signals)]
-    groups = [i for i in range(num_signals)]
+    groups = ["LFP", "LFP", "LFP", "LFP"] + [i for i in range(num_signals - 4)]
 
     # The sampling rate in Hz of each signal
     sampling_rate = [250] * num_signals
