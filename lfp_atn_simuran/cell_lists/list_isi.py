090e5edd0411c4e67d7f7438fe9e078ed1717987
Sean Martin <seankieran.m@hotmail.com>
2021-06-16 15:16:13 +0100

    Clean up list isi function

--- lfp_atn_simuran/cell_lists/list_isi.py
+++ lfp_atn_simuran/cell_lists/list_isi.py
@@ -1,9 +1,61 @@
 import os
+from copy import deepcopy
 
 import simuran
-from simuran.analysis.custom.nc import stat_per_cell
+import numpy as np
 
 here = os.path.dirname(os.path.abspath(__file__))
+
+def stat_per_cell(recording):
+    output = {}
+    # To avoid overwriting what has been set to analyse
+    all_analyse = deepcopy(recording.get_set_units())
+
+    for unit, to_analyse in zip(recording.units, all_analyse):
+        loaded = False
+        if to_analyse is None:
+            continue
+        if len(to_analyse) == 0:
+            continue
+        if not loaded:
+            unit.load()
+            unit.units_to_use = to_analyse
+        out_str_start = str(unit.group)
+        if unit.underlying is None:
+            for cell in to_analyse:
+                output[out_str_start + "_" + str(cell)] = [
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                ]
+        else:
+            for cell in to_analyse:
+                if cell in unit.underlying.get_unit_list():
+                    unit.underlying.set_unit_no(cell)
+
+                    wave_prop = unit.underlying.wave_property()
+                    isi_data = unit.underlying.isi()
+                    results = unit.underlying.get_results()
+                    output[out_str_start + "_" + str(cell)] = [
+                        results["Mean width"],
+                        results["Mean Spiking Freq"],
+                        results["Median ISI"],
+                        results["Std ISI"],
+                        results["CV ISI"],
+                    ]
+                    unit.underlying.reset_results()
+                else:
+                    output[out_str_start + "_" + str(cell)] = [
+                        np.nan,
+                        np.nan,
+                        np.nan,
+                        np.nan,
+                        np.nan,
+                    ]
+    # pdf_file.close()
+    return output
 
 def main():
     cell_list = os.path.join(here, "CTRL_Lesion_cells_filled.xlsx")
