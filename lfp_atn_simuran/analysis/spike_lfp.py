a99e6667ab0f82dc0e2a0870595f5bfde5582218
Sean Martin <seankieran.m@hotmail.com>
2021-06-28 14:37:54 +0100

    Finish spike lfp

--- lfp_atn_simuran/analysis/spike_lfp.py
+++ lfp_atn_simuran/analysis/spike_lfp.py
@@ -4,6 +4,10 @@ import simuran
 import numpy as np
 
 from lfp_atn_simuran.analysis.lfp_clean import LFPClean
+from skm_pyutils.py_table import list_to_df
+from skm_pyutils.py_plot import UnicodeGrabber
+
+import matplotlib.pyplot as plt
 
 
 def recording_spike_lfp(recording, clean_method="avg", **kwargs):
@@ -11,6 +15,8 @@ def recording_spike_lfp(recording, clean_method="avg", **kwargs):
     lc = LFPClean(method=clean_method, visualise=False)
     fmin = 0
     fmax = 100
+    for i in range(len(recording.signals)):
+        recording.signals[i].load()
     signals_grouped_by_region = lc.clean(
         recording.signals, fmin, fmax, method_kwargs=clean_kwargs
     )["signals"]
@@ -60,17 +66,74 @@ def recording_spike_lfp(recording, clean_method="avg", **kwargs):
             spike_train = unit.underlying.get_unit_stamp()
             g_data = nc_sig.plv(spike_train, mode="bs", fwin=[0, 20])
             sta = g_data["STAm"]
-            sfc = g_data["SFC"]
+            sfc = g_data["SFCm"]
+            t = g_data["t"]
+            f = g_data["f"]
 
-            output[name_for_save] = [sta, sfc]
+            output[name_for_save] = [sta, sfc, t, f]
             unit.underlying.reset_results()
 
     return output
 
 
-# TODO finish this function
 def combine_results(info, extra):
-    data = info[0]
+    import os
+    import simuran
+    import seaborn as sns
+    from lfp_atn_simuran.analysis.parse_cfg import parse_cfg_info
 
-    print(data)
-    return
+    cfg = parse_cfg_info()
+    base_dir = cfg.get("cfg_base_dir")
+    img_format = cfg.get("image_format")
+
+    out_dir, filename = extra
+    base, ext = os.path.splitext(os.path.basename(filename))
+
+    new_list1 = []
+    new_list2 = []
+
+    for row in info.itertuples():
+        group = row.Directory[len(base_dir + os.sep) :][0]
+        if group == "C":
+            group = "Control"
+        elif group == "L":
+            group = "ATNx (Lesion)"
+        else:
+            raise ValueError("unsupported group {}".format(group))
+        sta = row.STA
+        sfc = row.SFC
+        t = row.Time
+        f = row.Frequency
+        for i in range(len(sta)):
+            new_list1.append([group, float(sta[i]), float(t[i])])
+        for i in range(len(sfc)):
+            new_list2.append([group, float(sfc[i]) / 100, float(f[i])])
+
+    headers1 = ["Group", "STA", "Time (s)"]
+    headers2 = ["Group", "SFC", "Frequency (Hz)"]
+    df1 = list_to_df(new_list1, headers=headers1)
+    df2 = list_to_df(new_list2, headers=headers2)
+
+    simuran.set_plot_style()
+
+    fig, ax = plt.subplots()
+    sns.lineplot(data=df1, x="Time (s)", y="STA", ax=ax, style="Group", hue="Group")
+    mc = UnicodeGrabber.get("micro")
+    ax.set_ylabel(f"Spike triggered average {mc}V")
+    name = "average_sta_sub"
+    fig.savefig(os.path.join(out_dir, name + "." + img_format))
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    sns.lineplot(
+        data=df2, x="Frequency (Hz)", y="SFC", ax=ax, style="Group", hue="Group"
+    )
+    ax.set_ylabel(f"Spike field coherence")
+    name = "average_sfc_sub"
+    fig.savefig(os.path.join(out_dir, name + "." + img_format))
+    plt.close(fig)
+
+    out_fname = os.path.join(out_dir, base + "__sta" + ext)
+    df1.to_excel(out_fname, index=False)
+    out_fname = os.path.join(out_dir, base + "__sfc" + ext)
+    df2.to_excel(out_fname, index=False)
--- lfp_atn_simuran/cell_lists/list_spike_lfp.py
+++ lfp_atn_simuran/cell_lists/list_spike_lfp.py
@@ -20,8 +20,8 @@ def main():
     base_dir = cfg["cfg_base_dir"]
     out_dir = os.path.abspath(os.path.join(here, "..", "sim_results", herename))
     os.makedirs(out_dir, exist_ok=True)
-    cell_list = os.path.join(here, "CTRL_Lesion_cells_filled.xlsx")
-    headers = ["STA", "SFC"]
+    cell_list = os.path.join(here, "CTRL_Lesion_cells_filled_eeg.xlsx")
+    headers = ["STA", "SFC", "Time", "Frequency"]
     simuran.analyse_cell_list(
         cell_list,
         spike_lfp,
